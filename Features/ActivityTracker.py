8eb2c9a0dabd2db1bf1a70049c990ab5b70dde95
U:/TheTanooki-reddit <example@example.com>
2018-12-25 12:58:17 -0800

    Added ActivityTracker basics. TODO: Leave a comment instead of printing to console.

--- Features/ActivityTracker.py
+++ Features/ActivityTracker.py
@@ -1,6 +1,6 @@
 from Features.Feature import Feature
 import praw
-
+import time
 # Activity Tracker by /u/TheTanooki-reddit
 # BETA - Be Careful!!!
 
@@ -8,23 +8,28 @@ class ActivityTracker(Feature):
     """
     Tracks which mods are active.
     """
-    # Mods that are excluded
-    excludedMods = []
+    # Mods that are excluded - UNIMPLEMENTED
+    # excludedMods = []
     hoursOfInactivityPermitted = 24
     def check_condition(self):
         return True
-    def __init__(self):
-        run()
+    def __init__(self, reddit, subreddit_name):
+        self.reddit = reddit
+        self.subreddit_name = subreddit_name
+        # Mods that are excluded - UNIMPLEMENTED
+        # self.excludedMods = []
+        self.hoursOfInactivityPermitted = 24
+
     def getAllMods(self):
         modList = []
-        for mod in subreddit.moderator():
+        for mod in self.reddit.subreddit(self.subreddit_name).moderator():
             modList.append(mod)
         return modList
-    def run(self):
-        mods = getAllMods()
+    def perform_action(self):
+        mods = ActivityTracker.getAllMods(self)
         for moderator in mods:
-            for entry in reddit.subreddit(subreddit_name).mod.log(mod=moderator, limit=1):
+            for entry in self.reddit.subreddit(self.subreddit_name).mod.log(mod=moderator, limit=1):
                 UNIXTimestamp = entry.created_utc
-                if (time.time() - (hoursOfInactivityPermitted * 3600)) > UNIXTimestamp:
+                if (time.time() - (self.hoursOfInactivityPermitted * 3600)) > UNIXTimestamp:
                     # Mod has been inactive for too long
-                    print("/u/" + moderator + " has been inactive for too long")
+                    print("/u/" + moderator.name + " has been inactive for too long")
