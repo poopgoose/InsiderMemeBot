9fd3b30d8fa8c14b0f8a6fc9371c7a5eb50e7df9
/u/TheTanooki-reddit <example@example.com>
2019-01-04 08:32:42 -0800

    Basic activity tracker

--- Features/ActivityTracker.py
+++ Features/ActivityTracker.py
@@ -8,28 +8,38 @@ class ActivityTracker(Feature):
     """
     Tracks which mods are active.
     """
-    # Mods that are excluded - UNIMPLEMENTED
-    # excludedMods = []
-    hoursOfInactivityPermitted = 24
+    ## Be careful-not fully impl'd yet
     def check_condition(self):
         return True
     def __init__(self, reddit, subreddit_name):
         self.reddit = reddit
         self.subreddit_name = subreddit_name
         # Mods that are excluded - UNIMPLEMENTED
-        # self.excludedMods = []
-        self.hoursOfInactivityPermitted = 24
+        self.excludedMods = []
+        self.remindedMods = []
+        self.remindedRBPMods = []
 
     def getAllMods(self):
         modList = []
         for mod in self.reddit.subreddit(self.subreddit_name).moderator():
-            modList.append(mod)
+            if mod.name not in self.excludedMods:
+                modList.append(mod)
         return modList
+    def remindMod(self, moderator):
+        self.reddit.redditor(moderator.name).message('Inactivity on r/' + self.subreddit_name, "You've been inactive for 24 hours on r/" + self.subreddit_name)
+        self.remindedMods.append(moderator.name)
+    def remindRBP(self, moderator):
+        self.reddit.redditor('RoseBladePhantom').message('User inactivity', "u/" + moderator.name + " has been inactive for 48 hours on r/" + self.subreddit_name)
+        self.remindedRBPMods.append(moderator)
     def perform_action(self):
         mods = ActivityTracker.getAllMods(self)
         for moderator in mods:
             for entry in self.reddit.subreddit(self.subreddit_name).mod.log(mod=moderator, limit=1):
                 UNIXTimestamp = entry.created_utc
-                if (time.time() - (self.hoursOfInactivityPermitted * 3600)) > UNIXTimestamp:
-                    # Mod has been inactive for too long
-                    print("/u/" + moderator.name + " has been inactive for too long")
+                if (time.time() - (24 * 3600)) > UNIXTimestamp and moderator not in self.remindedMods:
+                    # Mod has been inactive for too long - PM them
+                    ActivityTracker.remindMod(self, moderator)
+                else:
+                    self.remindedMods.remove(moderator)
+                if (time.time() - (48 * 3600)) > UNIXTimestamp and moderator not in self.remindedRBPMods:
+                    ActivityTracker.remindRBP(self,moderator)
