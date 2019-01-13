2c60332428da60faf479ca03d8d695b1be5bb663
/u/TheTanooki-reddit <example@example.com>
2019-01-13 10:25:34 -0800

    ActivityTracker still broken

--- Features/ActivityTracker.py
+++ Features/ActivityTracker.py
@@ -1,6 +1,7 @@
 from Features.Feature import Feature
 import praw
 import time
+import json
 # Activity Tracker by /u/TheTanooki-reddit
 # BETA - Be Careful!!!
 
@@ -14,11 +15,26 @@ class ActivityTracker(Feature):
     def __init__(self, reddit, subreddit_name):
         self.reddit = reddit
         self.subreddit_name = subreddit_name
-        # Mods that are excluded - UNIMPLEMENTED
+        # Mods that are excluded
         self.excludedMods = []
-        self.remindedMods = []
-        self.remindedRBPMods = []
+        self.remindedMods = self.getRemindedMods()
+        self.remindedRBPMods =  self.getRemindedRBPMods()
 
+    def getRemindedMods(self):
+        f = open("./remindedMods", "r")
+        st = json.load(f)
+        print(st)
+        f.close()
+        return st
+    def getRemindedRBPMods(self):
+        f = open("./remindedRBPMods", "r")
+        st = json.load(f)
+        f.close()
+        return st
+    def storeRemindedMods(self, remindedMods):
+        f = open("./remindedMods", "w")
+        json.dump(remindedMods, f)
+        f.close()
     def getAllMods(self):
         modList = []
         for mod in self.reddit.subreddit(self.subreddit_name).moderator():
@@ -26,20 +42,27 @@ class ActivityTracker(Feature):
                 modList.append(mod)
         return modList
     def remindMod(self, moderator):
-        self.reddit.redditor(moderator.name).message('Inactivity on r/' + self.subreddit_name, "You've been inactive for 24 hours on r/" + self.subreddit_name)
-        self.remindedMods.append(moderator.name)
+        if moderator.name not in self.remindedMods:
+            self.reddit.redditor(moderator.name).message('Inactivity on r/' + self.subreddit_name, "You've been inactive for 24 hours on r/" + self.subreddit_name)
+            self.remindedMods.append(moderator.name)
+            ActivityTracker.storeRemindedMods(self,self.remindedMods)
     def remindRBP(self, moderator):
-        self.reddit.redditor('RoseBladePhantom').message('User inactivity', "u/" + moderator.name + " has been inactive for 48 hours on r/" + self.subreddit_name)
-        self.remindedRBPMods.append(moderator)
+        if moderator.name not in self.remindedMods:
+            self.reddit.redditor('RoseBladePhantom').message('User inactivity', "u/" + moderator.name + " has been inactive for 48 hours on r/" + self.subreddit_name)
+            self.remindedRBPMods.append(moderator.name)
+            ActivityTracker.storeRemindedMods(self, self.remindedRBPMods)
     def perform_action(self):
         mods = ActivityTracker.getAllMods(self)
         for moderator in mods:
             for entry in self.reddit.subreddit(self.subreddit_name).mod.log(mod=moderator, limit=1):
                 UNIXTimestamp = entry.created_utc
-                if (time.time() - (24 * 3600)) > UNIXTimestamp and moderator not in self.remindedMods:
+                if (time.time() - (24 * 3600)) > UNIXTimestamp and moderator.name not in self.remindedMods:
                     # Mod has been inactive for too long - PM them
                     ActivityTracker.remindMod(self, moderator)
-                else:
-                    self.remindedMods.remove(moderator)
-                if (time.time() - (48 * 3600)) > UNIXTimestamp and moderator not in self.remindedRBPMods:
+                    #print(self.remindedMods)
+                    print(moderator.name)
+                #if (time.time() - (24 * 3600)) < UNIXTimestamp:
+                    #self.remindedMods.remove(moderator)
+                if (time.time() - (48 * 3600)) > UNIXTimestamp and moderator.name not in self.remindedRBPMods:
                     ActivityTracker.remindRBP(self,moderator)
+                    print(moderator.name)
